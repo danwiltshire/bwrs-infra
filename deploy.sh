16f2790911301b751298bf821b4694755b57b795
danwiltshire <35703524+danwiltshire@users.noreply.github.com>
2021-03-12 08:37:11 +0000

    Deployable tmpl

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 # Usage: ./deploy.sh [prod/dev/etc]
 #
 # ~/.aws/config
@@ -6,18 +8,39 @@
 #   [profile prod]
 #       role_arn = arn:aws:iam::123456789012:role/AccessRole
 #       source_profile = default
+#       region = eu-west-1
 #   [profile dev]
 #       role_arn = arn:aws:iam::123456789012:role/AccessRole
 #       source_profile = default
+#       region = eu-west-1
+
+CFN_TEMPLATE="infrastructure.yml"
+APPLICATION="bwrs"
+ENVIRONMENT="$1"
 
 function log_usage {
     echo "Usage: ./deploy.sh [prod/dev/etc]"
 }
 
-if [ -z "$1" ]; then
+function lint {
+    cfn-lint "$1"
+}
+
+function deploy {
+    aws cloudformation deploy \
+        --template-file "$1" \
+        --stack-name "$APPLICATION-$2" \
+        --profile $2 \
+        --parameter-overrides \
+          Application=$APPLICATION \
+          Environment=$2
+}
+
+# Ensure an enviroment is specified
+if [ -z "$ENVIRONMENT" ]; then
   echo "Environment name not supplied!"
   log_usage
   exit 1
 fi
 
-aws cloudformation deploy --template-file infrastructure.yml --stack-name "bwrs-$1" --profile $1
+lint "$CFN_TEMPLATE" && deploy "$CFN_TEMPLATE" "$ENVIRONMENT"
